5b60bf40ad0615f972044eb534ba0ec9852709b3
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-01 21:07:14 +0330

    Keep ONLY the Duplicates

--- sets.py
+++ sets.py
@@ -45,4 +45,25 @@ print('thisset_remove_item -- lenght=> ' , len(thisset)  ,' \n type=>' , type(th
 thisset.clear()
 print('thisset_clear_all_set -- lenght=> ' , len(thisset)  ,' \n type=>' , type(thisset)  ,'\n data =>' , thisset , '\n')
 
+# Join Two Sets
+# You can use the union() method that returns a new set containing all items from both sets, or the update() method that inserts all the items from one set into another:
+
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+
+# The union() method returns a new set with all items from both sets:
+set3 = set1.union(set2)
+
+# The update() method inserts the items in set2 into set1:
+set1.update(set2)
+print('joined_two_sets -- lenght=> ' , len(set1)  ,' \n type=>' , type(set1)  ,'\n data =>' , set1 , '\n')
+
+# Keep ONLY the Duplicates
+# The intersection_update() method will keep only the items that are present in both sets.
+
+x = {"apple", "banana", "cherry"}
+y = {"google", "microsoft", "apple"}
+
+x.intersection_update(y)
+print('intersection_item_duplicated -- lenght=> ' , len(x)  ,' \n type=>' , type(x)  ,'\n data =>' , x , '\n')
 
