a89521e126d27c0ff75e5702a5706e38bb34294a
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-02 23:25:19 +0330

    Accessing Items

--- dicts.py
+++ dicts.py
@@ -33,3 +33,14 @@ print('getKeys -- lenght=> ' , len(getKeys)  ,' \n type=>' , type(getKeys)  ,'\n
 getValues = me.values()
 print('getValues -- lenght=> ' , len(getValues)  ,' \n type=>' , type(getValues)  ,'\n data =>' , getValues , '\n')
 
+# Get Items
+# The items() method will return each item in a dictionary, as tuples in a list.
+
+getItems = me.items()
+print('getItems -- lenght=> ' , len(getItems)  ,' \n type=>' , type(getItems)  ,'\n data =>' , getItems , '\n')
+
+# Check if Key Exists
+# To determine if a specified key is present in a dictionary use the in keyword:
+
+if 'relsID' in me:
+    print('Yes, "relsID" is one of the keys in the me dictionary')
