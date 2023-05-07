de05be1e39287f1daee809f9d2922cdebd53f31f
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-07 21:59:27 +0330

    The pass Statement

--- ifelse.py
+++ ifelse.py
@@ -54,3 +54,21 @@ b = 33
 c = 500
 if a > b and c > a:
   print("Both conditions are True")
+
+# Or
+# The or keyword is a logical operator, and is used to combine conditional statements:  
+
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
+
+# The pass Statement
+# if statements cannot be empty, but if you for some reason have an if statement with no content, put in the pass statement to avoid getting an error.
+
+a = 33
+b = 200
+
+if b > a:
+  pass
