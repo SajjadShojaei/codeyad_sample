c0311797d2b9abc38f2623730f06efe0050bffec
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-07 22:11:21 +0330

    The continue Statement

--- while.py
+++ while.py
@@ -16,3 +16,13 @@ while i < 6:
   if i == 3:
     break
   i += 1
+
+# The continue Statement
+# With the continue statement we can stop the current iteration, and continue with the next:
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)  
