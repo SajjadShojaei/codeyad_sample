a062c385616a68c9047894cdc07a76a2ac85c603
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-10 21:36:30 +0330

    for loop

--- examples/for_loop_exmpl.py
+++ examples/for_loop_exmpl.py
@@ -1,9 +1,17 @@
 names = ['amir' , 'sajjad' , 'ali' , 'majid' , 'farid' , 'mahdi' , 'arshia' , 'sajii']
-
+families = ['sajii' , 'shojaei' , 'khosravi' , 'hashemi' , 'amir']
 b = []
 
+
 for name in names:
-    if name[-1] == 'i':
-        b.append(name)
+    for family in families:
+        if name == family:
+            b.append(name)
 
 print(b)
+
+# for name in names:
+#     if name[-1] == 'i':
+#         b.append(name)
+
+# print(b)
