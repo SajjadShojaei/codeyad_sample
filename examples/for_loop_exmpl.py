6efd0cb6a6eb19afb826bb3933951d82508d5f32
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-12 02:11:54 +0330

    CODEGPT REFACTOR TESTING

--- examples/for_loop_exmpl.py
+++ examples/for_loop_exmpl.py
@@ -20,11 +20,30 @@ for n in name:
     if n not in b:
         print(f"your name has {name.count(n)} {n}")
         b.append(n)
-      
-
 
 # for name in names:
 #     if name[-1] == 'i':
 #         b.append(name)
 
 # print(b)
+
+
+#                +++++++++++ THIS IS CODE REFACTORED BT CODEGPT +++++++++++
+
+# names = ['amir', 'sajjad', 'ali', 'majid', 'farid', 'mahdi', 'arshia', 'sajii']
+# families = ['sajii', 'shojaei', 'khosravi', 'hashemi', 'amir']
+
+# b = [name for name in names if name in families]
+# print(b)
+
+# name = input("Enter your full name: ")
+# name = name.lower().replace(" ", "")
+
+# b = []
+
+# for n in set(name):
+#     count = name.count(n)
+#     print(f"Your name has {count} {n}")
+#     b.append(n)
+     
+
