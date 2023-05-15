015d23b856c180a550c069e8dad709eb14218e6e
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-15 21:23:08 +0330

    Functions

---
+++ functions.py
@@ -0,0 +1,9 @@
+# -------------[Functions]----------------
+# A function is a block of code which only runs when it is called.
+# You can pass data, known as parameters, into a function.
+# A function can return data as a result.
+
+def welcomeMessage():
+    print("Hello, My name is Sajji and i hope enjoy my first function in python")
+
+welcomeMessage()    
