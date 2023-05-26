ef39b7724a7dfb3fbe805a9486cfb1bb04342748
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-26 20:18:23 +0330

    Use lambda in map() function

--- lambda.py
+++ lambda.py
@@ -4,7 +4,7 @@
 
 x = lambda a , b : a + b 
 
-print(x(14 , 14))
+print(x(14 , 14)) # -> 28
 
 # Why Use Lambda Functions?
 # The power of lambda is better shown when you use them as an anonymous function inside another function.
@@ -14,4 +14,10 @@ def myFunc(n):
 
 myDoubler = myFunc(2)
 
-print(myDoubler(3))
+print(myDoubler(3)) # -> 6
+
+
+# Use lambda in map() function
+mylist = [1,2,3,5,6,7,8,9]
+z = map(lambda a : a * 2 , mylist)
+print(list(z)) # -> [2, 4, 6, 10, 12, 14, 16, 18]
