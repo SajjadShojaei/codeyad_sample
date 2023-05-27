11c42c5f712855db2292d7d664316f28c6c5c3c2
Sajjad Shojaei Moghaddam <86704788+SajjadShojaei@users.noreply.github.com>
2023-05-27 23:13:47 +0330

    car number

--- examples/classes_car_sample.py
+++ examples/classes_car_sample.py
@@ -1,10 +1,13 @@
 class Car:
     
+    cars_number = 0
+
     def __init__(self , name , model , color):
         self.name = name
         self.model = model
         self.color = color
         self.status = False
+        Car.cars_number += 1
 
     def starting(self):
         if self.status == False:
@@ -21,7 +24,12 @@ class Car:
             print(f"Your {self.name} is off, Please starting first.")
 
 c1 = Car('LandRover' , 'Defender' , 'Black')
+c2 = Car('Mersedes Benz' , 's500' , 'white')
+c3 = Car('bently' , '1414' , 'Dark Green')
+
 c1.starting()                                
 c1.starting() 
 c1.off()                               
-c1.off()                               
+c1.off()
+
+print(Car.cars_number) # -> 3 ,beacuse we create 3 cars objects (c1, c2, c3) and calling init() func for 3.
